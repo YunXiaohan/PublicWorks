c37787cc460fefb301973242b4d1f8edc474740b
YunXiaohan <Yunxiaohan96@icloud.com>
2024-06-25 16:23:29 +0900

    Update Update2Python.py

--- Update2Python.py
+++ Update2Python.py
@@ -1,5 +1,8 @@
 import subprocess
 import pkg_resources
+import time
+
+start = time.time()
 
 packages = [dist.project_name for dist in pkg_resources.working_set]
 for package in packages:
@@ -7,6 +10,13 @@ for package in packages:
 
 python_version =  subprocess.call("python --version", shell=True)
 
+end = time.time()
+spend_time = end - start
+
 print()
 print("User Python Version: ", python_version)
+print("Update Time: ", spend_time)
 print("Packages Updated Complete")
+
+# for update pip
+# C:\Users\user\AppData\Local\Programs\Python\Python312\python.exe -m pip install --upgrade pip
