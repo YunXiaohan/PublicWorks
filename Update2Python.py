7265b46d2aba778929641e4b681152fd9bc51758
YunXiaohan <Yunxiaohan96@icloud.com>
2024-06-25 16:10:24 +0900

    Update Update2Python.py

--- Update2Python.py
+++ Update2Python.py
@@ -1,9 +1,12 @@
 import subprocess
 import pkg_resources
 
-subprocess.call("python --version", shell=True)
-
 packages = [dist.project_name for dist in pkg_resources.working_set]
 for package in packages:
     subprocess.call(f"pip install --upgrade {package}", shell=True)
 
+python_version =  subprocess.call("python --version", shell=True)
+
+print()
+print("User Python Version: ", python_version)
+print("Packages Updated Complete")
