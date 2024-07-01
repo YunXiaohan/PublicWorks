c2e6521e25235e4355223798d10e559a6a0c2db1
YunXiaohan <Yunxiaohan96@icloud.com>
2024-07-01 09:26:05 +0900

    Update Practice2Test.py

--- Practice2Test.py
+++ Practice2Test.py
@@ -14,7 +14,12 @@ spec.loader.exec_module(lumapi)
 
 # 설계는 여기서
 fdtd = lumapi.FDTD()
-fdtd.addrect(x=0, y=0, z=0, x_span=1e-6, y_span=1e-6, z_span=1e-6)
+
+fdtd.addfdtd(x=0, y=0, z=0,x_span=1e-6, y_span=1e-6, z_span=3e-6)
+fdtd.addrect(x=0, y=0, z=0, x_span=0.5e-6, y_span=0.5e-6, z_span=1e-6)
+fdtd.addplane(x=0, y=0, z=-1e-6, x_span=2e-6, y_span=2e-6)
+fdtd.addpower(x=0, y=0, z=-1.5e-6, x_span=2e-6, y_span=2e-6)
+fdtd.save("open_py")
 fdtd.run()
 
 # 무한루프, 이거 필요없을지도
